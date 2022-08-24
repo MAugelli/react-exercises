c1dd88bb88f8f2722086f23a183387f03c55a9a6
MAugelli <m.augelli2@gmail.com>
2022-08-24 17:03:29 +0200

    components-06

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,9 @@
 const Hello = () => {
-    return ( <h1>Hello, World</h1> );
+    return ( <div>
+        <h1>Hello, World</h1> 
+        <p>What a beautiful day!</p>
+        </div>
+     );   
 }
  
 export default Hello;

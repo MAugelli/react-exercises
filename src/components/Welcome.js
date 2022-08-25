eae218942b63d1bd87bc353a331ecd5f86c958af
MAugelli <m.augelli2@gmail.com>
2022-08-25 18:20:44 +0200

    conditional-rendering-02

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import Age from "./Age";
 function Welcome(props){
     return (<div>
         <p>Hello, {props.name}!</p>  
-        {props.age>18 && <Age age={props.age}/> }  
+        {props.age && <Age age={props.age}/> }  
     </div>
     );   
 }

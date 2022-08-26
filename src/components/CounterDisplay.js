1fefd0c78215c92ad27f6de9442f1b2a6d8ffc91
MAugelli <m.augelli2@gmail.com>
2022-08-26 18:27:12 +0200

    fix state-04

---
+++ src/components/CounterDisplay.js
@@ -0,0 +1,10 @@
+import Counter from "./Counter";
+
+function CounterDisplay(props) {
+    return ( <h1>
+        Counter: <Counter initialValue={props.initialValue} incrementBy={props.incrementBy} timeOut={props.timeOut}/> 
+    </h1> 
+    )        
+}
+
+export default CounterDisplay;

0a4e3318f7a8e6573c8aa484d830cf108aa48dab
MAugelli <m.augelli2@gmail.com>
2022-08-30 17:19:25 +0200

    forms-02

---
+++ src/components/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Login extends React.Component {
+    state = {
+        username: "",
+        password: "",
+        remember: false
+    }
+
+    handleImputChange = (event) =>{
+        const value = event.target.value
+        const name = event.target.name
+        const type = event.target.type
+        const checked = event.target.checked
+
+        this.setState({
+            [name]:type === "checkbox" ? checked : value
+        })
+    }
+ 
+    render() {
+        return (
+            <div>
+                <h2>Login Form</h2>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleImputChange}/>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleImputChange}/>
+                <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleImputChange}/>
+            </div>
+        );
+    }
+}
+
+export default Login;

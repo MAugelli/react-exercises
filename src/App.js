bf430cf13b5a804143ed4c221a1f2f1cf0df0e5d
Matteo Augelli <104018555+MAugelli@users.noreply.github.com>
2022-09-07 16:33:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Counter from "./components/Counter";
 const App = () => {
   return (
     <div>
-    <Counter initialValue={0} incrementBy={1} timeOut={2000}/>
+    <Counter />
   </div>);
 }
 

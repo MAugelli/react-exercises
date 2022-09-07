182ff584ec670e280e388c44373f6e892f9cd22b
MAugelli <m.augelli2@gmail.com>
2022-09-07 22:12:22 +0200

    render-props-01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDoList from "./components/TodoList";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ToDoList
+    render={item => {
+      const propsItem = ["raba 1", "roba2", "roba3"]
+      
+      return (
+        <ul>{propsItem.map((item ) =>
+                    <li key={item + Math.random()}  >{item}  </li>
+                )
+                } </ul>
+      )
+    }
+    }
+    > 
+      
+    </ToDoList>
   );
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class ToDoList extends React.Component {
+
+    state = {
+        items: ["Go to gym", "Repair the car", "Watch react lesson"],
+        index:0
+    }
+
+    handleAddItem = (event) => {
+        event.preventDefault()
+        {event.target.elements.ToDo.value !== "" && this.setState((prevState) => ({
+            // items: prevState.items.concat(event.target.elements.ToDo.value)
+            items: [...prevState.items, event.target.elements.ToDo.value] 
+        }), 
+            () => {
+                event.target.elements.ToDo.value = "" 
+            } 
+        )}
+    }
+
+    handleClearList = (event) => {
+        event.preventDefault()
+        this.setState(() => ({
+            items: ""
+        }))
+    }
+
+    handleRemoveItem = (event) => {
+        const newItems = this.state.items
+        newItems.splice(event.target.name, 1)
+        this.setState(() => ({
+            items: newItems
+        }))
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleAddItem}>
+                    <input type="text" name="ToDo" />
+                    <button type="submit">Add</button>
+                    <button type="reset" onClick={this.handleClearList}>Clear list</button>
+                </form>
+                <div>
+                    {this.props.render}
+                </div>
+            </div>);
+    }
+}
+
+export default ToDoList;

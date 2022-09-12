50570c79420cdac5e6926d251915358375ebb177
MAugelli <m.augelli2@gmail.com>
2022-09-12 17:24:51 +0200

    custom-hooks-04

---
+++ src/components/GithubUser.js
@@ -0,0 +1,16 @@
+import useGithubUser from "./useGithubUser";
+
+function GithubUser({username}) {
+
+    const {user, error, loading} = useGithubUser(username)
+
+
+    return ( 
+    <div>
+        {loading && <h1>Caricamento...</h1>}
+        {user && <h1>{user.name}</h1>}
+        {error && <h1>C'è stato un errore</h1>}
+    </div> );
+}
+
+export default GithubUser;

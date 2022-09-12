b8c3adce5a49c7ceaf06d100e73363df00f38de0
MAugelli <m.augelli2@gmail.com>
2022-09-12 17:23:07 +0200

    custom-hooks-03

--- src/components/useGithubUser.js
+++ src/components/useGithubUser.js
@@ -2,32 +2,33 @@ import { useEffect, useState } from "react"
 
 function useGithubUser(username) {
     const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState(null)
 
     async function fetchGithubUser(username){
-        setError(null)
-        setLoading(true)
+        // setError(null)
+        // setLoading(true)
 
-        try {
+        // try {
             const responce = await fetch(`https://api.github.com/users/${username}`) 
             const json = await responce.json()
     
             setUser(json)
 
-        } catch (error) {
-            setError(error)
-            setUser(null)
-        } finally {
-            setLoading(false)
-        }
+        // } catch (error) {
+        //     setError(error)
+        //     setUser(null)
+        // } finally {
+        //     setLoading(false)
+        // }
     }
 
     useEffect (() => {
         fetchGithubUser(username)
     }, [username])
 
-    return{user, error, loading}
+    // return{user, error, loading}
+    return{user}
     
 }
 

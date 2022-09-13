1f31028d8d5000c7c4b4df7a2c52944ff8d8c430
MAugelli <m.augelli2@gmail.com>
2022-09-13 21:13:39 +0200

    useMemo

--- src/components/FilteredList.js
+++ src/components/FilteredList.js
@@ -4,28 +4,25 @@ import data from "./data"
 function FilteredList() {
     const [users, setUsers] = useState([])
 
-    const list = data.filter(user => {return user.age > 18 })
+    const list = data.filter(user => { return user.age > 18 })
 
     // useMemo(() => setUsers((users) => list))
-    useEffect(()=>{
+    useEffect(() => {
         setUsers((users) => list)
 
     }, [])
-    
+
     console.log(users);
     return (
         <div>
             {users.map(user => {
-                    return (
-                        <div key={Math.random()}>
-                            <h3>Id:{user.id}</h3>
-                             <h3>Name:{user.name}</h3>
-                             <h3>Age:{user.age}</h3>
-                        </div>
-                    )
-                }
-                )
-            }
+                return (
+                    <div key={Math.random()}>
+                        <h3>Id:{user.id}</h3>
+                        <h3>Name:{user.name}</h3>
+                        <h3>Age:{user.age}</h3>
+                    </div>
+                )})}
         </div >
     );
 }
